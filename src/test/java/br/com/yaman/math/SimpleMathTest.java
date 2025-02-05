a5f3214f667affb7d1d344df0fcecb41de23881f
Marcos Silva <marcos.silva@yaman.com.br>
2025-02-05 11:20:56 -0300

    Testando mais operações

--- src/test/java/br/com/yaman/math/SimpleMathTest.java
+++ src/test/java/br/com/yaman/math/SimpleMathTest.java
@@ -7,13 +7,11 @@ import org.junit.jupiter.api.Test;
 import br.com.yaman.SimpleMath;
 
 
-
 class SimpleMathTest {
 
 	@Test
 	void testSum() {
-		float x = 7f;
-		double y = 6.2d;
+
 		SimpleMath math = new SimpleMath();
 		double firstNumber = 6.2D;
 		double secondNumber = 2D;
@@ -24,10 +22,80 @@ class SimpleMathTest {
 		assertEquals(expected, actual, 
 		() -> firstNumber + "+" + secondNumber + 
 		"The testSum() did not produce" + expected + "result!");
-
-		
-		
+	
 		
 	}
-
+	
+	@Test
+	void testSubtraction() {
+		
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 5D;
+		double secondNumber = 5D;
+		
+		Double result = math.subtraction(firstNumber, secondNumber);
+		
+		double expected = 0;
+		assertEquals(expected, result, () -> firstNumber + "-" + secondNumber + 
+				"The testSubtraction() did not produce" + expected + "result!" );
+	}
+	
+	@Test
+	void testMultiplication() {
+		
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 5D;
+		double secondNumber = 5D;
+		
+		Double result = math.multiplication(firstNumber, secondNumber);
+		
+		double expected = 25;
+		assertEquals(expected, result, () -> firstNumber + "*" + secondNumber + 
+				"The testMultiplication() did not produce" + expected + "result!");
+	}
+	
+	@Test
+	void testDivision() {
+		
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 5D;
+		double secondNumber = 5D;
+		
+		Double result = math.division(firstNumber, secondNumber);
+		
+		double expected = 1;
+		assertEquals(expected, result, 
+				() -> firstNumber + "/" + secondNumber + 
+				"The testDivision() did not produce" + expected + "result!");
+	}
+	
+	@Test
+	// Mean AVG
+	void testMean() {
+		
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 5D;
+		double secondNumber = 5D;
+		
+		Double result = math.mean(firstNumber, secondNumber);
+		
+		double expected = 5;
+		assertEquals(expected, result,  
+				() -> firstNumber + "AVG" + secondNumber + 
+				"The testMean() did not produce" + expected + "result!");
+	}
+	
+	@Test
+	void testSquareRoot() {
+		
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 9D;
+		
+		
+		Double result = math.squareRoot(firstNumber);
+		
+		double expected = 3;
+		assertEquals(expected, result, () -> firstNumber + 
+				"The testSquareRoot() did not produce" + expected + "result!");
+	}
 }
