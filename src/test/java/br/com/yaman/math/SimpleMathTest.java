182f3f616a86f08eada0bcd56269068cb55b6897
Marcos Silva <marcos.silva@yaman.com.br>
2025-02-05 15:18:33 -0300

    Criando um Template de Código com BDD Style

--- src/test/java/br/com/yaman/math/SimpleMathTest.java
+++ src/test/java/br/com/yaman/math/SimpleMathTest.java
@@ -14,15 +14,20 @@ class SimpleMathTest {
 	@Test
 	@DisplayName("Test 6.2 + 2 = 8.2")
 	void testSum_When_SixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo() {
-
+		
+		// AAA Arrange, Act Assert
+		// Given Arrange
 		SimpleMath math = new SimpleMath();
+		
 		double firstNumber = 6.2D;
 		double secondNumber = 2D;
-		
-		Double actual = math.sum(firstNumber, secondNumber);
-	
 		double expected = 8.2D;
-		assertEquals(expected, actual, 
+		
+		// When Act
+		Double actual = math.sum(firstNumber, secondNumber);
+		
+		// Then Assert
+				assertEquals(expected, actual, 
 		() -> firstNumber + "+" + secondNumber + 
 		"The testSum() did not produce" + expected + "result!");
 	
