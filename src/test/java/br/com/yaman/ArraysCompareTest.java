c34977b80717fcd0c31246d1dae8425ef035bbc6
Marcos Silva <marcos.silva@yaman.com.br>
2025-02-21 14:21:58 -0300

    Comparativo de Arrays | assertArrayEquals

---
+++ src/test/java/br/com/yaman/ArraysCompareTest.java
@@ -0,0 +1,22 @@
+package br.com.yaman;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class ArraysCompareTest {
+
+	@Test
+	void test() {
+		int[] numbers = {25,8,21,32,3};
+		int[] expectedArray = {3,8,21,25,32};
+		
+		Arrays.sort(numbers);
+		
+		assertArrayEquals(numbers, expectedArray);
+	}
+
+}
